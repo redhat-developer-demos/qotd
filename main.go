eae459fc3c4a88db6b41f490c21587e74385f01e
Don Schenck <don.schenck@gmail.com>
2020-09-25 14:14:31 -0400

    Added my own quote

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ var quotes = Quotes{
 	Quote{Id: 2, Quotation: "Life is really simple, but we insist on making it complicated.", Author: "Confucius"},
 	Quote{Id: 3, Quotation: "This above all, to thine own self be true.", Author: "William Shakespeare"},
 	Quote{Id: 4, Quotation: "Never complain. Never explain.", Author: "Katharine Hepburn"},
-	Quote{Id: 5, Quotation: "Do be do be dooo.", Author: "Frank Sinatra"},
+	Quote{Id: 5, Quotation: "It is not only what you do, but also the attitude you bring to it, that makes you a success.", Author: "Don Schenck"},
 }
 
 func main() {
