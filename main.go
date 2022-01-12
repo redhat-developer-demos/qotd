bda08a6a482b2114414c40a67fecdca9eabd6285
Don Schenck <don.schenck@gmail.com>
2022-01-12 13:04:44 -0500

    Using a quote from The Dude, man

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 var quotes = Quotes{
 	Quote{Id: 0, Quotation: "I got a fever, and the only prescription is more cowbell!", Author: "Will Ferrell"},
-	Quote{Id: 1, Quotation: "Knowledge is power.", Author: "Francis Bacon"},
+	Quote{Id: 1, Quotation: "Yeah, well, that's just like, your opinion ... man.", Author: "The Dude"},
 	Quote{Id: 2, Quotation: "Life is really simple, but we insist on making it complicated.", Author: "Confucius"},
 	Quote{Id: 3, Quotation: "This above all, to thine own self be true.", Author: "William Shakespeare"},
 	Quote{Id: 4, Quotation: "Never complain. Never explain.", Author: "Katharine Hepburn"},
